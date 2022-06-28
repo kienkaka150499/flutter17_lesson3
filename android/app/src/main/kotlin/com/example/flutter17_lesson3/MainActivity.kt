9fd7552ce1b9ea2feeacfde44efb8b23d312120c
kienkaka150499 <75771331+kienkaka150499@users.noreply.github.com>
2022-06-28 19:29:56 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter17_lesson3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter17_lesson3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homework_lesson2.dart
@@ -0,0 +1,12 @@
+import 'dart:math';
+
+void main(){
+  var numbers=[];
+  for(var i=1;i<=1000;i++){
+    int random=-1000+ Random().nextInt(2000);
+    numbers.add(random);
+  }
+
+  numbers.sort((a,b)=>b.compareTo(a));
+  print(numbers.last);
+}
